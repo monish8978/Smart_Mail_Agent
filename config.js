60ded021d1a5a768659987cc344974f15dd6ffda
Neha Rani <neha.rani@iam.c-zentrix.com>
2026-09-15 13:00:33 +0530

    Initial commit - Smart Mail Agent Frontend

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.__APP_CONFIG__ = {
-  API_URL: "http://localhost:8024",
-  WS_URL: "ws://localhost:8024/ws"
+  API_URL: window.location.protocol + "//" + "172.16.3.215" + ":8024",
+  WS_URL: (window.location.protocol === "https:" ? "wss://" : "ws://") + "172.16.3.21" + ":8024/ws"
 };
